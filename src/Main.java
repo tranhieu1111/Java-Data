785329d67d4e2af59c8f6081b66283612e718afd
Xuan Lam Pham <phamxuanlam@gmail.com>
2024-03-13 21:21:22 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
       
         
-        System.out.println("Hello world!");  //Sửa câu lệnh sau
+        println("Hello world!");  //Sửa câu lệnh sau
 
 
         
